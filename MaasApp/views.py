cd2d93d6f52e876b6aafa4bad11f3f9d5852c366
DDuijker <djoekeduijker@live.nl>
2023-11-08 16:55:49 +0100

    created all_user template, you can now view all users by email at /all_users

--- MaasApp/views.py
+++ MaasApp/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.template import loader
+from .models import User
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("Hello World!")
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render())
+
+
+def all_users(request):
+    """Loads the all_users template with an object containing the users object"""
+    users = User.objects.all().values()
+    template = loader.get_template('all_users.html')
+    context = {
+        'users': users
+    }
+    return HttpResponse(template.render(context, request))
