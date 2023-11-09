6462daf74396d95ca8b848b58ab08a8dfbe8b6da
DDuijker <djoekeduijker@live.nl>
2023-11-09 15:50:33 +0100

    Added test view

--- MaasApp/urls.py
+++ MaasApp/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('users', views.all_users, name='users'),
-    path('users/details/<int:request_id>', views.user_details, name="details")
+    path('users/', views.all_users, name='users'),
+    path('users/details/<int:request_id>', views.user_details, name='details'),
+    path('testing/', views.testing, name='testing'),
 ]
--- MaasApp/views.py
+++ MaasApp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from .models import User
+
+
 # Create your views here.
 
 
@@ -26,5 +28,13 @@ def user_details(request, request_id):
     template = loader.get_template('user_details.html')
     context = {
         'user': user
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def testing(request):
+    template = loader.get_template('template.html')
+    context = {
+        'fruits': ['Apple', 'Banana', 'Cherry'],
     }
     return HttpResponse(template.render(context, request))
