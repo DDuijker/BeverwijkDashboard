f5543fa69aed49c76fa1edef1c2ef1130a9398e9
DDuijker <djoekeduijker@live.nl>
2023-11-15 22:56:46 +0100

    commit before pull

--- BeverwijkDashboard/settings.py
+++ BeverwijkDashboard/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'MaasApp',
     'bootstrap5',
+    'django_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
