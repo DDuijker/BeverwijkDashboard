942aa4009f406e1f2e43fc0311a57d58c49a9589
Djoeke <djoekeduijker@live.nl>
2023-11-15 11:14:15 +0100

    installed packages

--- BeverwijkDashboard/settings.py
+++ BeverwijkDashboard/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'MaasApp',
-    'bootstrap5',
+    'django_bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
