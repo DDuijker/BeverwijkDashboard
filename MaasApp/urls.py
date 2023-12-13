f0071ae693a2675b06130637c8a931bfb3fe58ad
MarjonPhu <marjonphu15@gmail.com>
2023-12-13 14:31:40 +0100

    added gis map

--- MaasApp/urls.py
+++ MaasApp/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
@@ -12,4 +13,5 @@ urlpatterns = [
     path('overig-ov', views.busses, name='busses'),
     path('testing/', views.testing, name='testing'),
     path('enquete/', views.enquete, name='enquete'),
+    path('components/gismap/roadmap/roadmap/index.html', TemplateView.as_view(template_name='components/gismap/roadmap/roadmap/index.html'), name='roadmap_index')
 ]
