199e1f0049155ad107173e4b6a306e5d5d4cee86
MarjonPhu <60743844+MarjonPhu@users.noreply.github.com>
2023-12-22 14:28:30 +0100

    filled dashboard + css fixes

--- MaasApp/views.py
+++ MaasApp/views.py
@@ -40,6 +40,7 @@ def index(request):
         circle_categories, circle_values, "Verdeling vervoersmiddelen"
     )
 
+    # --------------------SCOOTER GRAFIEKEN----------------------------
     bar_charts = []
     for data_set in scooter_data:
         chart = GraphGeneratorService.generate_bar_chart(
@@ -50,10 +51,95 @@ def index(request):
         )
         bar_charts.append(chart)
 
+    # ------------------------ENQUETE GRAFIEKEN-----------------------------
+    all_answers = EnqueteResponse.objects.all()
+    satisfaction = []
+    considered_maas_options = []
+    input_data = []
+
+    for answer in all_answers:
+        considered_maas_options.append(answer.consider_shared_mobility)
+        satisfaction_value = answer.satisfaction_transport_modes.strip()
+        input_data.append(answer.most_used_transport_modes)
+        if satisfaction_value:
+            try:
+                rating = int(float(satisfaction_value))
+                satisfaction.append(rating)
+            except ValueError:
+                # Handle the case when the value is not a valid number
+                pass
+
+    avg_satisfaction = round((float(sum(satisfaction) / len(satisfaction)) if satisfaction else 0.0), 2)
+
+    # Count occurrences of each maas_option
+    maas_options_counts = Counter(considered_maas_options)
+
+    # Extract categories and values for the pie chart
+    maas_options = list(maas_options_counts.keys())
+    maas_options_counts = Counter(considered_maas_options)
+
+    # Remove Nvt from option counts
+    for key in list(maas_options_counts):
+        if key == 'Nvt':
+            del maas_options_counts[key]
+    print(maas_options_counts)
+
+    maas_options_values = [count for option, count in maas_options_counts.items() if option != 'Nvt']
+
+    circle_diagram = GraphGeneratorService.generate_pie_chart(
+        categories=maas_options_counts,
+        values=maas_options_values,
+        title="Meeste interesse in deelmobiliteit:"
+    )
+
+    data_dict = {}
+
+    for line in input_data:
+        modes_and_percentages = [mode.strip() for mode in line.strip(';').split(';')]
+
+        for index, mode in enumerate(modes_and_percentages):
+            # Create a dictionary entry for each mode
+            if mode not in data_dict:
+                data_dict[mode] = 0
+
+            # Accumulate rankings (in reverse order, so the most used gets the highest ranking)
+            data_dict[mode] += len(modes_and_percentages) - index
+
+    # Calculate the total count of all rankings
+    total_rankings = sum(data_dict.values())
+
+    # Convert rankings to percentages
+    percentage_data_dict = {mode: (count / total_rankings) * 100 for mode, count in data_dict.items()}
+
+    # Sort the dictionary by values (total percentages) in descending order
+    sorted_percentage_data_dict = dict(sorted(percentage_data_dict.items(), key=lambda item: item[1], reverse=True))
+
+    # Calculate y_ticks based on the range from the lowest to the highest percentage
+    max_percentage = int(max(sorted_percentage_data_dict.values()))
+    y_ticks = list(range(0, max_percentage + 10, 5))
+
+    # Remove the parentheses from the keys
+    keys = [GenerateColorService.remove_words_in_parentheses(key) for key in sorted_percentage_data_dict.keys()]
+
+    chart_data = GraphGeneratorService.generate_bar_chart(
+        categories=keys,
+        values=list(sorted_percentage_data_dict.values()),
+        x_label="Vervoerswijzen",
+        y_label="Totaal gebruik (%)",
+        title="Meest gebruikte vervoerswijzen",
+        y_ticks=y_ticks,
+        grid=True,
+        grid_alpha=0.5,
+        size=(12, 5),
+        color='#F0F0F0'
+    )
     context = {
         'line_graph': graph,
         'circle_chart': circle_chart,
-        **{f'graph_{i + 1}': chart for i, chart in enumerate(bar_charts)}
+        **{f'graph_{i + 1}': chart for i, chart in enumerate(bar_charts)},
+        'avg_satisfaction': avg_satisfaction,
+        'circle_diagram': circle_diagram,
+        'chart_data': chart_data
     }
     template = loader.get_template('index.html')
     return HttpResponse(template.render(context, request))
